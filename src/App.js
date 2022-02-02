3227d3ddbcdcb22d9c5656a5455fa71ddf0c2d64
mandykparson <79546411+mandykparson@users.noreply.github.com>
2022-02-02 13:12:33 -0700

    adding counter and api comps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import Header from './Header/Header';
+import API from './Components/API';
+import Counter from './Components/Counter';
+import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Header/>
+        <div className='Subs'>
+          <div className='sub'>
+            <Counter />
+          </div>
+          <div className='sub'>
+            <API />
+          </div>
+        </div>
     </div>
   );
 }
